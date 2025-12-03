4fc7444ecccc3777b000e6d645bcfd7afb667c64
Stephen <stephen.lu20@gmail.com>
2025-12-03 11:44:19 -0500

    Added GET and DELETE mapping for user

--- api/src/main/java/com/planner/api/controller/UserController.java
+++ api/src/main/java/com/planner/api/controller/UserController.java
@@ -3,17 +3,27 @@ import org.springframework.web.bind.annotation.*;
 import com.planner.api.dto.CreateUserDto;
 import com.planner.api.dto.UpdateUserDto;
 import com.planner.api.entity.User;
+import com.planner.api.repository.UserRepository;
 import com.planner.api.service.UserService;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class UserController {
+
+    private final UserRepository userRepository;
 
     private final UserService userService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
     }
+
+    @GetMapping("/{id}")
+        public User getUser(@PathVariable Long id) {
+            return userRepository.findById(id)
+                    .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        }
 
     @PostMapping
     public User createUser(@RequestBody CreateUserDto dto) {
@@ -24,5 +34,13 @@ public class UserController {
     public User updateUser( @PathVariable Long id,
                             @RequestBody UpdateUserDto dto) {
         return userService.modifyUser(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+            User user = userRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("User not found"));
+            
+            userRepository.delete(user);
     }
 }
