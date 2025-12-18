4543ceff84757c565f98ed73c4fdaebb30788b83
Stephen <stephen.lu20@gmail.com>
2025-12-18 14:49:07 -0500

    API get requests on button push

--- vueJS/src/App.vue
+++ vueJS/src/App.vue
@@ -2,12 +2,43 @@
   <div>
     <h1>API Viewer</h1>
 
-    <button>Load Users</button>
-    <button>Load Templates</button>
-    <button>Load Activities</button>
+    <button @click="fetchData('/user')">Load Users</button>
+    <button @click="fetchData('/template')">Load Templates</button>
+    <button @click="fetchData('/activity')">Load Activities</button>
 
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        <pre>{{ formatItem(item) }}</pre>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    }
+  },
+  methods: {
+    async fetchData(endpoint) {
+      try {
+        const response = await fetch(`http://localhost:8080${endpoint}`);
+        if (!response.ok) {
+          throw new Error(`HTTP error ${response.status}`);
+        }
+        this.items = await response.json();
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    formatItem(item) {
+      return JSON.stringify(item, null, 2);
+    }
+  }
+}
+</script>
 
 <style>
 button {
