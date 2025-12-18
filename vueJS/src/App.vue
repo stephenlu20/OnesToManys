0ca405decf23a438546d33c906d2e070d29bb08d
Stephen <stephen.lu20@gmail.com>
2025-12-18 14:50:11 -0500

    Added more styles, copied from vanilla JS css

--- vueJS/src/App.vue
+++ vueJS/src/App.vue
@@ -51,4 +51,28 @@ pre {
   padding: 8px;
   border: 1px solid #ddd;
 }
+
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif;
+  line-height: 1.6;
+  color: #222;
+  max-width: 50rem;
+  padding: 2rem;
+  margin: auto;
+  background: #fafafa;
+}
+
+img {
+  max-width: 100%;
+}
+
+a {
+  color: #5eb5f4;
+}
+
+h1,
+h2,
+strong {
+  color: #1e60ca;
+}
 </style>
